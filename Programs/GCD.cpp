c3656a80228c9c4ba19a27894e515e03751031bb
DarthSid1603 <43276914+DarthSid1603@users.noreply.github.com>
2019-10-06 14:10:13 +0530

    GCD.cpp

---
+++ Programs/GCD.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b) // using Euclidean algorithm
+{ 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b);  
+      
+} 
+
+int main()
+{
+  int a,b;
+  cout<<"Enter two numbers\n";
+  cin>>a>>b;
+  int g = gcd (a,b);
+  cout<<"GCD of "<<a<<' '<<b<<" is "<<g<<'\n';
+}
