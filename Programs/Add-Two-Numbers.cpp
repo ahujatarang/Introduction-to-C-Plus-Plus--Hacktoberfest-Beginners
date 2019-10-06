f876506df74cffcbc130686bcf70f57c1595af6b
snehaahuja145 <56220263+snehaahuja145@users.noreply.github.com>
2019-10-06 19:02:28 +0530

    Created Add-Two-Numbers.cpp

---
+++ Programs/Add-Two-Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, s;
+    
+    cout << "Enter two integers: "<<endl;
+    cin >> a;
+    cin>> b;
+    cout<<endl;
+   
+    s= a+b;
+    
+    cout << a << " + " <<  b << " = " << s;     
+    return 0;
+}
