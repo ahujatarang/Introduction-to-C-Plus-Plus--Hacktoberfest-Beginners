e30b8444b9f8d3fa405fcc7a5f8a796b1f3609b8
mdshahansha <mdshahansha44411@gmail.com>
2020-10-24 23:06:36 +0530

    added modularity in code

--- Programs/Add-Two-Numbers.cpp
+++ Programs/Add-Two-Numbers.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 using namespace std;
+int sum(int a,int b)
+{
+    int c=0;
+    c=a+b;
+    return c;
+}
 int main()
 {
     int a, b, s;
-    
+
     cout << "Enter two integers: "<<endl;
     cin >> a;
     cin>> b;
     cout<<endl;
-   
-    s= a+b;
-    
-    cout << a << " + " <<  b << " = " << s;     
+
+    s=sum(a,b);
+
+    cout << a << " + " <<  b << " = " << s;
     return 0;
 }
